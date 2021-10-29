a54d8e352dbc8c3906d6f7ea37328f58652105ab
EdvinWik <edvin.wik@elev.ga.ntig.se>
2021-10-29 13:11:29 +0200

    🤡 🌱

--- Lan-Edvin.py
+++ Lan-Edvin.py
@@ -1,1 +1,15 @@
-.
+e = input("Är det lan dags: [Ja] [Nej] : ")
+
+if e.lower() == "ja":
+    print("Bra, så ska det se ut.")
+else:
+    print("Stick, dörren är där.")
+
+# e = ''
+# Ja = "Bra, så ska det se ut."
+# Nej = "Then you shall perish."
+
+# if e == Ja:
+#     print(f"{Ja}")
+# else:
+#     print(f"{Nej}")
